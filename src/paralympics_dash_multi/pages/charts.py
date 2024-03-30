6d6fa1a551b2ec8e8ff93a0b45788691904f781c
4jjnaomi <115657585+4jjnaomi@users.noreply.github.com>
2024-03-30 16:27:03 +0000

    fixed multi_app and added 1 test

--- src/paralympics_dash_multi/pages/charts.py
+++ src/paralympics_dash_multi/pages/charts.py
@@ -1,15 +1,13 @@
 # Line and bar charts page
 from dash import register_page
 import dash_bootstrap_components as dbc
-from paralympics_dash_multi import layout_charts
-
-import src.paralympics_dash.layout_elements
+from paralympics_dash_multi import layout_charts as lc
 
 # register the page in the app
 register_page(__name__, name="Charts", title="Charts")
 
 # The rows are in a separate Python module called layout_charts.py
 layout = dbc.Container([
-    src.paralympics_dash.layout_elements.row_one,
-    src.paralympics_dash.layout_elements.row_two,
+    lc.row_one,
+    lc.row_two,
 ])
--- src/paralympics_dash_multi/pages/events.py
+++ src/paralympics_dash_multi/pages/events.py
@@ -1,15 +1,14 @@
 # Page with the map and stats card
 from dash import register_page
 import dash_bootstrap_components as dbc
-from paralympics_dash_multi import layout_events
+from paralympics_dash_multi import layout_events as le
 
-import src.paralympics_dash.layout_elements
 
 # register the page in the app
 register_page(__name__, name='Events', title='Events', path="/", )
 
 # The rows are in a separate Python module called layout_events.py
 layout = dbc.Container([
-    src.paralympics_dash.layout_elements.row_one,
-    src.paralympics_dash.layout_elements.row_two,
+    le.row_one,
+    le.row_two,
 ])
